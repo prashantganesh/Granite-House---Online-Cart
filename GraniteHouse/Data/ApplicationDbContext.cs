a570f6516343d1aa0d34ecb4b889e0961ebc6120
prashantganesh <prashantganesh007@gmail.com>
2021-05-26 14:11:21 +0530

    Add project files.

---
+++ GraniteHouse/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using GraniteHouse.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraniteHouse.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProductTypes> ProductTypes { get; set; }
+    }
+}
